99718ed0cd88592857a08d76e4a1a1a5b872fd86
TehVulpez <vulpezlederp@gmail.com>
2023-03-09 19:56:04 -0600

    added support for aliases

--- stats/stats.js
+++ stats/stats.js
@@ -43,10 +43,12 @@ function updateTable(stats) {
 	document.getElementById("gets").textContent = "Total gets: " + gets.toLocaleString();
 	document.getElementById("assists").textContent = "Total assists: " + assists.toLocaleString();
 	
-	if (counts > 0) {
+	if (counts) {
 		document.getElementById("none").style.display = "none";
 		document.getElementById("stats").style.display = "flex";
 	}
+	document.getElementById("hoc_title").textContent = "HoC for " + stats.username;
+	document.getElementById("hof_title").textContent = "HoF/Ho999 for " + stats.username;
 }
 
 function getTables(html) {
@@ -54,42 +56,78 @@ function getTables(html) {
 	return dom.parseFromString(html, "text/html").getElementsByTagName("tbody");
 }
 
-function addThread(username, title, html, stats) {
-	const tables = getTables(html);
+function sideHoF(hof, title, stats, name) {
+	const hofRows = Array.from(hof.rows).reverse(); // search starting from latest get
+	let gets = 0;
+	let assists = 0;
 	
-	if (tables.length > 1) { // only for sidethreads
-		const hof = tables[0];
-		const hofRows = Array.from(hof.rows).reverse(); // search starting from latest get
-		let gets = 0;
-		let assists = 0;
-		let getRow, assistRow;
-		
-		if ((getRow = hofRows.findIndex(row => row.cells[1].textContent.toLowerCase() == username)) > -1) { // gets
-			const getCell = hofRows[getRow].cells[2];
-			gets = getCell.textContent ? parseInt(getCell.textContent) : 1; // set to 1 if it's empty
-		}
-		if ((assistRow = hofRows.findIndex(row => row.cells[3].textContent.toLowerCase() == username)) > -1) { // assists
-			const assistCell = hofRows[assistRow].cells[4];
-			assists = assistCell.textContent ? parseInt(assistCell.textContent) : 1;
-		}
-		if (gets || assists)
-			stats.hof.push({"title":title, "gets":gets, "assists":assists});
+	const getRow = hofRows.findIndex(row => row.cells[1].textContent.toLowerCase() == name.toLowerCase());
+	if (getRow > -1) { // gets
+		const getCell = hofRows[getRow].cells[2];
+		gets = getCell.textContent ? parseInt(getCell.textContent) : 1; // set to 1 if it's empty
 	}
 	
-	const hoc = tables[tables.length-1]; // get last table on the page
-	const hocRows = Array.from(hoc.rows);
-	let row;
+	const assistRow = hofRows.findIndex(row => row.cells[3].textContent.toLowerCase() == name.toLowerCase())
+	if (assistRow > -1) { // assists
+		const assistCell = hofRows[assistRow].cells[4];
+		assists = assistCell.textContent ? parseInt(assistCell.textContent) : 1;
+	}
 	
-	if ((row = hocRows.findIndex(row => row.cells[1].textContent.toLowerCase() == username)) > -1) { // HoC
+	const stat = stats.hof.findIndex(stat => stat.title == title);
+	if (stat > -1) {
+		stats.hof[stat].gets += gets;
+		stats.hof[stat].assists += assists;
+	}
+	else if (gets || assists)
+		stats.hof.push({"title":title, "gets":gets, "assists":assists});
+}
+
+function getHoC(hoc, title, stats, name) {
+	const hocRows = Array.from(hoc.rows);
+	
+	const row = hocRows.findIndex(row => row.cells[1].textContent.toLowerCase() == name.toLowerCase());
+	if (row > -1) { // HoC
 		const rank = parseInt(hocRows[row].cells[0].textContent);
 		const counts = parseInt(hocRows[row].cells[2].textContent);
 		
-		stats.hoc.push({"title":title, "rank":rank, "counts":counts});
+		if (stats.username.toLowerCase() == name.toLowerCase()) // only for original name, not aliases
+			stats.username = hocRows[row].cells[1].textContent; // change to proper capitalization
+		
+		const stat = stats.hoc.findIndex(stat => stat.title == title);
+		if (stat > -1)
+			stats.hoc[stat].counts += counts; // add to existing record, in case counts already found under another name
+		else
+			stats.hoc.push({"title":title, "rank":rank, "counts":counts});
 		updateTable(stats);
 	}
 }
 
-function loadSides(username, html, stats) {
+function addThread(tables, title, name, stats) {
+	if (tables.length > 1) { // only for sidethreads
+		const hof = tables[0];
+		sideHoF(hof, title, stats, name);
+	}
+	
+	const hoc = tables[tables.length-1];
+	getHoC(hoc, title, stats, name);
+}
+
+function loadThread(title, html, stats, aliases) {
+	const tables = getTables(html);
+	const user = stats.username.toLowerCase();
+	
+	if (user in aliases) { // check every alias
+		for (let i in aliases[user]) {
+			const name = aliases[user][i];
+			addThread(tables, title, name, stats);
+		}
+	}
+	else {
+		addThread(tables, title, stats.username, stats);
+	}
+}
+
+function loadSides(html, stats, aliases) {
 	const tables = getTables(html);
 	for (let x = 0; x < tables.length; x++) { // there are several tables in the side thread stats page
 		const tbody = tables[x];
@@ -99,23 +137,41 @@ function loadSides(username, html, stats) {
 			const url = row.cells[0].childNodes[0].getAttribute('href');
 			fetch("https://old.reddit.com"+url+".json?raw_json=1", {mode:"cors", cache:"force-cache"}) // load stats page for each thread
 				.then(r => r.json())
-				.then(json => addThread(username, title, json.data.content_html, stats));
+				.then(json => loadThread(title, json.data.content_html, stats, aliases));
 		}
 	}
 }
 
-function mainHoF(username, html, stats) {
-	const hof = getTables(html)[0];
-	const rows = Array.from(hof.rows);
-	let row;
-	
-	if ((row = rows.findIndex(row => row.cells[1].textContent.toLowerCase() == username)) > -1) {
+function mainHoF(rows, name, stats) {
+	const row = rows.findIndex(row => row.cells[1].textContent.toLowerCase() == name.toLowerCase());
+	if (row > -1) {
 		const gets = parseInt(rows[row].cells[2].textContent);
 		const assists = parseInt(rows[row].cells[3].textContent);
 		
-		stats.hof.push({title:"Decimal", "gets": gets, "assists":assists});
+		const stat = stats.hof.findIndex(stat => stat.title == "Decimal");
+		if (stat > -1) {
+			stats.hof[stat].gets += gets;
+			stats.hof[stat].assists += assists;
+		}
+		else if (gets || assists)
+			stats.hof.push({title:"Decimal", "gets": gets, "assists":assists});
 		updateTable(stats);
 	}
+}
+
+function hallOfParticipation(html, stats, aliases) {
+	const hof = getTables(html)[0];
+	const rows = Array.from(hof.rows);
+	const user = stats.username.toLowerCase();
+	
+	if (user in aliases) {
+		for (let i in aliases[user]) {
+			const name = aliases[user][i];
+			mainHoF(rows, name, stats);
+		}
+	}
+	else
+		mainHoF(rows, user, stats);
 }
 
 function notFound(stats) {
@@ -123,29 +179,44 @@ function notFound(stats) {
 		document.getElementById("none").style.display = "block";
 }
 
-function getStats() {
+function getAliases(csv) {
+	let aliases = {};
+	const rows = csv.split("\n");
+	for (let y = 0; y < rows.length-1; y++) {
+		const names = rows[y].split(",");
+		for (let x = 0; x < names.length; x++) {
+			const name = names[x].toLowerCase();
+			aliases[name] = names;
+		}
+	}
+	return aliases;
+}
+
+async function getStats() {
 	const username = document.getElementById("username").value.replace(/\s/g, ""); // remove whitespace
 	if (username) {
-		let stats = {hoc:[], hof:[]};
+		let stats = {hoc:[], hof:[], "username":username};
 		updateTable(stats);
-		document.getElementById("hoc_title").textContent = "HoC for "+username;
-		document.getElementById("hof_title").textContent = "HoF/Ho999 for "+username;
 		document.getElementById("stats").style.display = "none";
 		document.getElementById("none").style.display = "none";
+		
+		const aliases = await fetch("https://raw.githubusercontent.com/cutonbuminband/rcounting/main/rcounting/counters/aliases.txt")
+			.then(r => r.text())
+			.then(getAliases);
 		
 		// Main thread HoC
 		fetch("https://old.reddit.com/r/counting/wiki/hoc.json?raw_json=1", {mode:"cors", cache:"force-cache"})
 			.then(r => r.json())
-			.then(json => addThread(username.toLowerCase(), "Decimal", json.data.content_html, stats));
+			.then(json => loadThread("Decimal", json.data.content_html, stats, aliases));
 		// Main thread HoF
 		fetch("https://old.reddit.com/r/counting/wiki/participation.json?raw_json=1", {mode:"cors", cache:"force-cache"})
 			.then(r => r.json())
-			.then(json => mainHoF(username.toLowerCase(), json.data.content_html, stats));
+			.then(json => hallOfParticipation(json.data.content_html, stats, aliases));
 		
 		// Sidethreads
 		fetch("https://old.reddit.com/r/counting/wiki/side_stats.json?raw_json=1", {mode:"cors", cache:"force-cache"})
 			.then(r => r.json())
-			.then(json => loadSides(username.toLowerCase(), json.data.content_html, stats));
+			.then(json => loadSides(json.data.content_html, stats, aliases));
 		
 		window.setTimeout(notFound, 3000);
 	}
